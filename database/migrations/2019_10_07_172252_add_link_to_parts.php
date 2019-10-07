6cb5353e2af5a5f1141b304cf73fa76b29e172b9
Ждан Жуланов <zhulanov@gmail.com>
2019-10-07 22:27:26 +0500

    fixes and migrations

---
+++ database/migrations/2019_10_07_172252_add_link_to_parts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLinkToParts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('parts', function (Blueprint $table) {
+            $table->string('link', 255)->after('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('parts', function (Blueprint $table) {
+            $table->dropColumn('link');
+        });
+    }
+}
